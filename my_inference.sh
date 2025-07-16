1d93236cd442a7cf7c452d3d8caabbda283ddd2f
Ice streams <46258228+IceStreams@users.noreply.github.com>
2025-07-16 10:26:19 +0800

    Add files via upload

---
+++ my_inference.sh
@@ -0,0 +1,11 @@
+###
+ # @Author: Zuoxibing
+ # @email: zuoxibing1015@163.com
+ # @Date: 2024-08-29 15:59:54
+ # @LastEditTime: 2025-02-14 11:13:30
+ # @Description: file function description
+### 
+
+CUDA_VISIBLE_DEVICES=0,1 python ST_SYSU_MyNet_Pred.py
+CUDA_VISIBLE_DEVICES=0,1 python ST_SECOND_MyNet_Pred.py
+CUDA_VISIBLE_DEVICES=0,1 python ST_LEVIR1024_MyNet_Pred.py
